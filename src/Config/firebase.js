98262ce5426457b905e323d864744da42e92dbc2
Joaquin <jaltable@fi.uba.ar>
2023-05-17 18:35:21 -0300

    i dont know what im doing

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -3,22 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVGOnZf96K29utbYO4dvMEQdHdRIZTOsk",
-  authDomain: "build-93979.firebaseapp.com",
-  projectId: "build-93979",
-  storageBucket: "build-93979.appspot.com",
-  messagingSenderId: "998667503227",
-  appId: "1:998667503227:web:6500c9072b86563d4e84de"
-};
-
-/*const firebaseConfig = {
   apiKey: "AIzaSyBPqv2koV2kJHFV5dCcvgjIWkdgZ46eUOE",
   authDomain: "portfolio-8f090.firebaseapp.com",
   projectId: "portfolio-8f090",
   storageBucket: "portfolio-8f090.appspot.com",
   messagingSenderId: "112888837427",
   appId: "1:112888837427:web:de9e11b3815a6de44b7a46"
-  };*/
+  };
 
   firebase.initializeApp(firebaseConfig);
   firebase.auth = firebase.auth();
